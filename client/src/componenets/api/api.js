983db88bbbda6a89ed09169dd077a3a39af0a206
bennyh960 <bennyh960@gmail.com>
2022-06-26 12:54:20 +0300

    fixed resources

--- client/src/componenets/api/api.js
+++ client/src/componenets/api/api.js
@@ -1,45 +1,39 @@
 import axios from "axios";
 
-// todo change url when moving from local to prodeuction
-// git push heroku main
-// const accountsAPI = axios.create({
-//   baseURL: "/accounts",
-// });
-// const usersAPI = axios.create({
-//   baseURL: "/users",
-// });
-// const oneUserAPI = axios.create({
-//   baseURL: "/users",
-// });
-// const createNewAccount = axios.create({
-//   baseURL: "/newAccount",
-// });
 //? ===================================================
 const accounts = axios.create({
-  baseURL: "http://localhost:5000/accounts",
+  // baseURL: "http://localhost:5000/accounts",
+  baseURL: "/accounts",
 });
 const usersAPI = axios.create({
-  baseURL: "http://localhost:5000/users",
+  // baseURL: "http://localhost:5000/users",
+  baseURL: "/users",
 });
 const oneUserAPI = axios.create({
-  baseURL: "http://localhost:5000/users",
+  // baseURL: "http://localhost:5000/users",
+  baseURL: "/users",
   // baseURL: "mongodb://127.0.0.1:27017/bankDB",
 });
 const oneAccount = axios.create({
-  baseURL: "http://localhost:5000/account",
+  // baseURL: "http://localhost:5000/account",
+  baseURL: "/account",
   // baseURL: "mongodb://127.0.0.1:27017/bankDB",
 });
 const createNewAccount = axios.create({
-  baseURL: "http://localhost:5000/accounts/newAccount",
+  // baseURL: "http://localhost:5000/accounts/newAccount",
+  baseURL: "/accounts/newAccount",
 });
 const createNewUser = axios.create({
-  baseURL: "http://localhost:5000/users/newUser",
+  // baseURL: "http://localhost:5000/users/newUser",
+  baseURL: "/users/newUser",
 });
 const updateAccount = axios.create({
-  baseURL: "http://localhost:5000/accounts/edit",
+  // baseURL: "http://localhost:5000/accounts/edit",
+  baseURL: "/accounts/edit",
 });
 const moneyAction = axios.create({
-  baseURL: "http://localhost:5000/admin/money-actions",
+  // baseURL: "http://localhost:5000/admin/money-actions",
+  baseURL: "/admin/money-actions",
 });
 
 const serverApi = {
